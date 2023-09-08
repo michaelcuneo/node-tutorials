f75bbdbfc187cd667e916abe6b11a8d2cbb36b45
Michael Cuneo <me@michaelcuneo.com.au>
2023-09-08 15:07:47 +1000

    WHAT

--- src/public/index.js
+++ src/public/index.js
@@ -1,10 +1,3 @@
-import "./style.css";
-
-// Needed for Hot Module Replacement
-if (typeof module.hot !== "undefined") {
-  module.hot.accept(); // eslint-disable-line no-undef
-}
-
 export const copyCommand = (id) => {
   navigator.clipboard.writeText(id);
 };
